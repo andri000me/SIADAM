2a66adc36366dcb3a667d213a64dc62df6684e90
angga dharma <anggadharma60>
2020-01-28 16:45:46 +0700

    landing page + login

--- application/views/login.php
+++ application/views/login.php
@@ -1,24 +1,23 @@
-<!DOCTYPE html>
 <html>
 <head>
 	
 	<title>SIADAM LOGIN</title>
-	<link rel="stylesheet" type="text/css" href="../../assets/css/login.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/') . 'login.css'?>">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-	<link rel="icon" type="image/png" sizes="32x32" href="../../assets/img/login/siadampng.png">
+	<link rel="icon" type="image/png" sizes="48x48" href="<?php echo base_url('assets/img/') . 'siadampng.png'?>">
 	
 </head>
 <body>
 	
-	<img class="wave" src="../../assets/img/login/wave.png">
+	<img class="wave" src="<?php echo base_url('assets/img/login/') . 'wave.png'?>">
 	<div class="container">
 		<div class="img">
-			<img src="../../assets/img/login/telkom.png">
+			<img src="<?php echo base_url('assets/img/login/') . 'telkom.png'?>">
 		</div>
 		<div class="login-content">
-			<form action="index.html" method="GET">
-				<img src="../../assets/img/login/siadampng.png">
+			<form action="" method="POST">
+				<img src="<?php echo base_url('assets/img/') . 'siadampng.png'?>">
 				<h2 class="title">SIADAM</h2>
 				<h4 class="title">Sistem Informasi</h4>
 				<h4 class="title">Access Data Management</h4>
@@ -46,10 +45,10 @@
             </form>
         </div>
     </div>
-	<script type="text/javascript" src="../../assets/js/login.js"></script>
+	<script type="text/javascript" src="<?php echo base_url('assets/js/') . 'login.js'?>"></script>
 	<script>
 		function goBack() {
-			window.location.href='landing_page.html';
+			window.location.href='<?php echo base_url()?>';
 		}
 	</script>
 </body>
