91d34810411cc7e7d8f5072172fb62d6406c5757
angga dharma <anggadharma60>
2020-02-03 11:22:35 +0700

    update

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -27,15 +27,10 @@ class Admin extends CI_Controller
 		check_not_login();
 		//check admin buat fungsi_helper
 		check_admin();
-<<<<<<< HEAD
     $this->load->model('Pegawai_model');
 	$this->load->model('Regional_model');
 	$this->load->model('STO_model');
-=======
-		$this->load->model('Pegawai_model');
-		$this->load->model('Regional_model');
->>>>>>> refs/remotes/origin/master
-		$this->load->library('form_validation');
+	$this->load->library('form_validation');
   }
 
   // Halaman Awal Admin
